419975ff6d19e7b1e706834c1722c1a9c5a15aac
Michal Pazio <elmiciu@gmail.com>
2023-03-21 19:53:01 +0100

    Init

---
+++ Exercise3/Exercise3/Controllers/StudentsController.cs
@@ -0,0 +1,48 @@
+﻿using Exercise3.Models.DTOs;
+using Exercise3.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercise3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IStudentsRepository _studentsRepository;
+        public StudentsController(IStudentsRepository studentsRepository)
+        {
+            _studentsRepository = studentsRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{index}")]
+        public async Task<IActionResult> Get(string index)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{index}")]
+        public async Task<IActionResult> Put(string index, StudentPUT newStudentData)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(StudentPOST newStudent)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{index}")]
+        public async Task<IActionResult> Delete(string index)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Exercise3/Exercise3/Services/FileDbService.cs
@@ -0,0 +1,46 @@
+﻿using Exercise3.Models;
+
+namespace Exercise3.Services
+{
+    public interface IFileDbService
+    {
+        public IEnumerable<Student> Students { get; set; }
+        Task SaveChanges();
+    }
+
+    public class FileDbService : IFileDbService
+    {
+        private readonly string _pathToFileDatabase;
+        public IEnumerable<Student> Students { get; set; } = new List<Student>();
+        public FileDbService(IConfiguration configuration)
+        {
+            _pathToFileDatabase = configuration.GetConnectionString("Default") ?? throw new ArgumentNullException(nameof(configuration));
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            if (!File.Exists(_pathToFileDatabase))
+            {
+                return;
+            }
+            var lines = File.ReadLines(_pathToFileDatabase);
+
+            var students = new List<Student>();
+
+            //Tutaj należy przeparsować dane ze zmiennej lines, tak jak w drugim zadaniu
+
+            Students = students;
+        }
+
+        public async Task SaveChanges()
+        {
+            await File.WriteAllLinesAsync(
+                _pathToFileDatabase, 
+                null //tutaj należy zapewnić listę stringów zawierającą odpowiednio sformatowane dane studentów
+                     // np. Jan,Kowalski,s1234,3/20/1991,Informatyka,Dzienne,kowalski@wp.pl,Jan,Anna
+                );
+        }
+
+    }
+}
